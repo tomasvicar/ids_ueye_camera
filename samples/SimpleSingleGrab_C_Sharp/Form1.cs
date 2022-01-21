f8959f5fae1aac74c3b183ac18ed74547901cf2e
Tomáš Vičar <vicar@PC-SE4158-716.ubmi.feec.vutbr.cz>
2022-01-21 16:51:24 +0100

    working example

---
+++ samples/SimpleSingleGrab_C_Sharp/Form1.cs
@@ -0,0 +1,109 @@
+﻿//===========================================================================//
+//                                                                           //
+//  Copyright (C) 2006 - 2017                                                //
+//  IDS Imaging GmbH                                                         //
+//  Dimbacherstr. 6-8                                                        //
+//  D-74182 Obersulm-Willsbach                                               //
+//                                                                           //
+//  The information in this document is subject to change without            //
+//  notice and should not be construed as a commitment by IDS Imaging GmbH.  //
+//  IDS Imaging GmbH does not assume any responsibility for any errors       //
+//  that may appear in this document.                                        //
+//                                                                           //
+//  This document, or source code, is provided solely as an example          //
+//  of how to utilize IDS software libraries in a sample application.        //
+//  IDS Imaging GmbH does not assume any responsibility for the use or       //
+//  reliability of any portion of this document or the described software.   //
+//                                                                           //
+//  General permission to copy or modify, but not for profit, is hereby      //
+//  granted,  provided that the above copyright notice is included and       //
+//  reference made to the fact that reproduction privileges were granted     //
+//  by IDS Imaging GmbH.                                                     //
+//                                                                           //
+//  IDS cannot assume any responsibility for the use or misuse of any        //
+//  portion of this software for other than its intended diagnostic purpose  //
+//  in calibrating and testing IDS manufactured cameras and software.        //
+//                                                                           //
+//===========================================================================//
+
+/// Developer Note: I tried to keep it as simple as possible.
+/// Therefore there are no functions asking for the newest driver software or freeing memory beforehand, etc.
+/// This program is solely meant to show one of the simplest ways to interact with an IDS Camera through the uEye API.
+/// (XS Cameras are not supported)
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sandkasten_CSharp
+{
+    public partial class SimpleSingleGrab_C_Sharp : Form
+    {
+        //To use the .NET interface you have to create a uEye .NET object
+        private uEye.Camera Camera = new uEye.Camera();
+        IntPtr DisplayHandle = IntPtr.Zero;
+
+        public SimpleSingleGrab_C_Sharp()
+        {
+            InitializeComponent();
+            DisplayHandle = DisplayWindow.Handle;
+
+            //Initialize the camera
+            Camera.Init();
+
+            //Allocate an default image memory
+            Camera.Memory.Allocate();
+
+            Camera.EventFrame += onFrameEvent;
+
+            //Capture a single image with
+            Camera.Acquisition.Freeze();
+        }
+        
+
+
+        private void onFrameEvent(object sender, EventArgs e)
+        {
+            uEye.Camera Camera = sender as uEye.Camera;
+
+            //Display the image on the screen
+            Camera.Display.Render( DisplayHandle, uEye.Defines.DisplayRenderMode.FitToWindow);
+        }
+
+
+
+        //The following are key definitions
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            //Free the allocated image memory and closes the program
+            Camera.Exit();
+            Close();
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            //Capture a single frame
+            Camera.Acquisition.Freeze();
+        }
+
+        private void Live_Click(object sender, EventArgs e)
+        {
+            //Capture a live image
+            Camera.Acquisition.Capture();
+
+            //Currently we are working without Ring-Buffer
+            //For the purpose of showing this example that's enough
+            //But if you want to do some Image data processing you'll need to adjust the settings
+        }
+
+        private void DisplayWindow_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ samples/my_run/simple_live_windows_forms_not_working/AcquisitionWorker.cs
@@ -0,0 +1,161 @@
+﻿/*!
+ * \file    AcquisitionWorker.cs
+ * \author  IDS Imaging Development Systems GmbH
+ * \date    2020-02-01
+ * \since   1.1.6
+ *
+ * \brief   The AcquisitionWorker class is used in a worker thread to capture
+ *          images from the device continuously and do an image conversion into
+ *          a desired pixel format.
+ *
+ * \version 1.1.0
+ *
+ * Copyright (C) 2020 - 2021, IDS Imaging Development Systems GmbH.
+ *
+ * The information in this document is subject to change without notice
+ * and should not be construed as a commitment by IDS Imaging Development Systems GmbH.
+ * IDS Imaging Development Systems GmbH does not assume any responsibility for any errors
+ * that may appear in this document.
+ *
+ * This document, or source code, is provided solely as an example of how to utilize
+ * IDS Imaging Development Systems GmbH software libraries in a sample application.
+ * IDS Imaging Development Systems GmbH does not assume any responsibility
+ * for the use or reliability of any portion of this document.
+ *
+ * General permission to copy or modify is hereby granted.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+namespace simple_live_windows_forms
+{
+    public class AcquisitionWorker
+    {
+        // Event which is raised if a new image was received
+        public delegate void ImageReceivedEventHandler(object sender, Bitmap image);
+        public event ImageReceivedEventHandler ImageReceived;
+
+        // Event which is raised if the counters has changed
+        public delegate void CountersUpdatedEventHandler(object sender, uint frameCounter, uint errorCounter);
+        public event CountersUpdatedEventHandler CountersUpdated;
+
+        // Event which is raised if an Error or Exception has occurred
+        public delegate void MessageBoxTriggerEventHandler(object sender, String messageTitle, String messageText);
+        public event MessageBoxTriggerEventHandler MessageBoxTrigger;
+
+        private peak.core.DataStream dataStream;
+        private peak.core.NodeMap nodeMapRemoteDevice;
+
+        private bool running;
+        private uint frameCounter;
+        private uint errorCounter;
+
+        public AcquisitionWorker()
+        {
+            Debug.WriteLine("--- [AcquisitionWorker] Init");
+            running = false;
+            frameCounter = 0;
+            errorCounter = 0;
+        }
+
+        public void Start()
+        {
+            Debug.WriteLine("--- [AcquisitionWorker] Start Acquisition");
+            try
+            {
+                // Lock critical features to prevent them from changing during acquisition
+                nodeMapRemoteDevice.FindNode<peak.core.nodes.IntegerNode>("TLParamsLocked").SetValue(1);
+
+                // Start acquisition
+                dataStream.StartAcquisition();
+                nodeMapRemoteDevice.FindNode<peak.core.nodes.CommandNode>("AcquisitionStart").Execute();
+                nodeMapRemoteDevice.FindNode<peak.core.nodes.CommandNode>("AcquisitionStart").WaitUntilDone();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("--- [AcquisitionWorker] Exception: " + e.Message);
+                MessageBoxTrigger(this, "Exception", e.Message);
+            }
+
+            running = true;
+
+            while (running)
+            {
+                try
+                {
+                    // Get buffer from device's datastream
+                    var buffer = dataStream.WaitForFinishedBuffer(1000);
+
+                    // Create IDS peak IPL
+                    var iplImg = new peak.ipl.Image((peak.ipl.PixelFormatName)buffer.PixelFormat(), buffer.BasePtr(),
+                        buffer.Size(), buffer.Width(), buffer.Height());
+
+                    // Debayering and convert IDS peak IPL Image to RGB8 format
+                    iplImg = iplImg.ConvertTo(peak.ipl.PixelFormatName.BGR8);
+
+                    var width = Convert.ToInt32(iplImg.Width());
+                    var height = Convert.ToInt32(iplImg.Height());
+                    var stride = Convert.ToInt32(iplImg.PixelFormat().CalculateStorageSizeOfPixels(iplImg.Width()));
+
+                    // Queue buffer so that it can be used again 
+                    dataStream.QueueBuffer(buffer);
+
+                    var image = new Bitmap(width, height, stride,
+                        System.Drawing.Imaging.PixelFormat.Format24bppRgb, iplImg.Data());
+
+                    // Create a deep copy of the Bitmap, so it doesn't use memory of the IDS peak IPL Image.
+                    // Warning: Don't use image.Clone(), because it only creates a shallow copy!
+                    var imageCopy = new Bitmap(image);
+
+                    // The other images are not needed anymore.
+                    image.Dispose();
+                    iplImg.Dispose();
+
+                    if (ImageReceived != null)
+                    {
+                        Debug.WriteLine("--- [AcquisitionWorker] Send image Nr. " + (frameCounter + 1));
+                        ImageReceived(this, imageCopy);
+                    }
+
+                    frameCounter++;
+                }
+                catch (Exception e)
+                {
+                    errorCounter++;
+                    Debug.WriteLine("--- [AcquisitionWorker] Exception: " + e.Message);
+                    MessageBoxTrigger(this, "Exception", e.Message);
+                }
+
+                // Raise event with current frame and error counter
+                CountersUpdated(this, frameCounter, errorCounter);
+            }
+        }
+
+        public void Stop()
+        {
+            Debug.WriteLine("--- [AcquisitionWorker] Stop Acquisition");
+            running = false;
+        }
+
+        public void SetDataStream(peak.core.DataStream dataStream)
+        {
+            Debug.WriteLine("--- [AcquisitionWorker] Set dataStream");
+            this.dataStream = dataStream;
+        }
+
+        public void SetNodemapRemoteDevice(peak.core.NodeMap nodeMap)
+        {
+            Debug.WriteLine("--- [AcquisitionWorker] Set nodeMap");
+            nodeMapRemoteDevice = nodeMap;
+        }
+    }
+}
