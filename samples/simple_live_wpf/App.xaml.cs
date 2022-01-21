dbcc9e09deedd98e84dd7e7710a9b2e2c1a3ea5a
Tomáš Vičar <vicar@PC-SE4158-716.ubmi.feec.vutbr.cz>
2022-01-21 15:40:11 +0100

    upload samples

---
+++ samples/simple_live_wpf/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿/*!
+ * \file    App.xaml.cs
+ * \author  IDS Imaging Development Systems GmbH
+ * \date    2020-02-01
+ * \since   1.1.6
+ *
+ * \brief   This application demonstrates how to use the IDS peak API
+ *          combined with a WPF GUI to display images from Genicam
+ *          compatible device.
+ *
+ * \version 1.0.0
+ *
+ * Copyright (C) 2020 - 2021, IDS Imaging Development Systems GmbH.
+ *
+ * The information in this document is subject to change without notice
+ * and should not be construed as a commitment by IDS Imaging Development Systems GmbH.
+ * IDS Imaging Development Systems GmbH does not assume any responsibility for any errors
+ * that may appear in this document.
+ *
+ * This document, or source code, is provided solely as an example of how to utilize
+ * IDS Imaging Development Systems GmbH software libraries in a sample application.
+ * IDS Imaging Development Systems GmbH does not assume any responsibility
+ * for the use or reliability of any portion of this document.
+ *
+ * General permission to copy or modify is hereby granted.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace simple_live_wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThread]
+        static void Main()
+        {
+            App app = new App();
+            MainWindow mainWindow = new MainWindow();
+            if (!mainWindow.HasError())
+            {
+                app.Run(mainWindow);
+            }
+        }
+    }
+}
